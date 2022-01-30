4f096a081599079ddf122dff38a853b53dca87fd
xenxynon <flynryder427@gmail.com>
2022-01-30 16:03:16 +0000

    lavender: use pixel offline charging animation
    Signed-off-by: xenxynon <flynryder427@gmail.com>

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018-2019 The LineageOS Project
+#Copyright (C) 2018-2019 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -322,6 +322,12 @@ PRODUCT_PACKAGES += \
     libOmxVdec \
     libOmxVenc \
     libstagefrighthw
+
+
+# Pixel Offline Charger
+PRODUCT_PACKAGES+= \
+    charger_res_images \
+    product_charger_res_images
 
 # Perfd (dummy)
 PRODUCT_PACKAGES += \
